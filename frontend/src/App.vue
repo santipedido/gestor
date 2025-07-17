7d485388cc6624a54b0d76f8e5b0a3e764d330d4
santipedido <pedidos028@gmail.com>
2025-07-17 11:11:39 -0500

    Habilitar solo el módulo de clientes para pruebas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,15 @@
 <template>
-  <div style="text-align:center; margin-top: 50px;">
-    <h1>Ping Pong Frontend ↔ Backend</h1>
-    <button @click="pingBackend">Probar conexión</button>
-    <p v-if="mensaje">{{ mensaje }}</p>
-    <p v-if="error" style="color:red;">{{ error }}</p>
+  <div>
+    <Clientes />
   </div>
 </template>
 
 <script>
+import Clientes from './components/Clientes.vue'
+
 export default {
-  data() {
-    return {
-      mensaje: '',
-      error: ''
-    }
-  },
-  methods: {
-    async pingBackend() {
-      this.mensaje = '';
-      this.error = '';
-      try {
-        const res = await fetch(import.meta.env.VITE_API_URL + '/');
-        if (!res.ok) throw new Error('Error en la respuesta del backend');
-        const data = await res.json();
-        this.mensaje = data.mensaje || JSON.stringify(data);
-      } catch (e) {
-        this.error = e.message;
-      }
-    }
+  components: {
+    Clientes
   }
 }
 </script>
