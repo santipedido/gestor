6db26aa574793a7dec093478e60e58f6d9c3c7be
santipedido <pedidos028@gmail.com>
2025-07-17 11:56:39 -0500

    CORS definitivo: solo permitir Netlify

--- backend/main.py
+++ backend/main.py
@@ -8,9 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://pedidosq.netlify.app",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "https://pedidosq.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
